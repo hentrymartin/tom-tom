130b0e69f1106ee370e63effba97f280ddcaba23
Hentry Martin <hentrymartin.p@gmail.com>
2019-04-28 21:51:45 +0530

    Start the server on specific port

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,6 +1,6 @@
 export default {
   URL: 'http://localhost:3004/',
-  FE_URL: 'http://localhost:3000',
+  FE_URL: 'http://localhost:4444',
   // Two minutes polling interval
   POLLING_INTERVAL: 120000, 
 }
